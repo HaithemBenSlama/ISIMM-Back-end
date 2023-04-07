f190d918e32cfd5d5c11ffecffaa50d1dbc2ffa4
Haithem Ben Slama <haithem.benslama@hotmail.fr>
2023-04-07 21:03:01 +0100

    Modifying Query noteFindListeEtudiantAndNotesForSection

--- src/main/java/com/example/demo/Doa/NoteRepository.java
+++ src/main/java/com/example/demo/Doa/NoteRepository.java
@@ -13,18 +13,35 @@ import java.util.Map;
 @Repository
 public interface NoteRepository extends JpaRepository<Note, Long> {
 
-    @Query("Select new map(er.i as idEtudiant,er.c as cinEtudiant,er.n as nomEtudiant," +
-            "er.p as prenomEtudiant,nr.n as note,nr.t as typeNote) " +
-            "FROM (SELECT e.id as i,e.cin as c,e.nom as n,e.prenom as p FROM TD t " +
-            "JOIN Section s ON s.idSection=t.section.idSection JOIN Etudiant e ON e.td.idTd=t.idTd " +
-            "WHERE s.idSection=:group_id) AS er " +
-            "LEFT JOIN " +
-            "(SELECT n.etudiant.id as ei,n.note as n,n.type as t FROM Note n " +
-            "WHERE n.enseingant.id=:id_enseignant " +
-            "AND n.semestre.idSemestre=:id_semestre " +
-            "AND n.matiere.idMatiere=:id_matiere " +
-            "AND n.type=0 OR n.type=1) AS nr " +
-            "ON nr.ei=er.i")
+    @Query(
+            value = "SELECT er.id as idEtudiant, er.cin as cinEtudiant, er.nom as nomEtudiant, er.prenom as prenomEtudiant, " +
+                    "nr1.note as noteExam, " +
+                    "nr2.note as noteDs " +
+                    "FROM ( " +
+                    "   SELECT e.id as id, e.cin as cin, e.nom as nom, e.prenom as prenom " +
+                    "   FROM TD t " +
+                    "   JOIN Section s ON s.id_section = t.id_td " +
+                    "   JOIN Etudiant e ON e.id_td = t.id_td " +
+                    "   WHERE s.id_section = :group_id " +
+                    ") AS er " +
+                    "LEFT JOIN ( " +
+                    "   SELECT n1.id_etudiant as etudiant_id, n1.note as note, n1.type as type " +
+                    "   FROM Note n1 " +
+                    "   WHERE n1.id_enseingant = :id_enseignant " +
+                    "   AND n1.id_semestre = :id_semestre " +
+                    "   AND n1.id_matiere = :id_matiere " +
+                    "   AND n1.type = 1 " +
+                    ") AS nr1 ON nr1.etudiant_id = er.id " +
+                    "LEFT JOIN ( " +
+                    "   SELECT n2.id_etudiant as etudiant_id, n2.note as note, n2.type as type " +
+                    "   FROM Note n2 " +
+                    "   WHERE n2.id_enseingant = :id_enseignant " +
+                    "   AND n2.id_semestre = :id_semestre " +
+                    "   AND n2.id_matiere = :id_matiere " +
+                    "   AND n2.type = 0 " +
+                    ") AS nr2 ON nr2.etudiant_id = er.id ",
+            nativeQuery = true
+    )
     List<Map<String,Object>> noteFindListeEtudiantAndNotesForSection(
             Long id_enseignant,
             Long group_id,
