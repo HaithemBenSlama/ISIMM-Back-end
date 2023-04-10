f37581c22dbb7f46d72a87a2dad233aeed1f9684
miledyessine <miled.yassine7@gmail.com>
2023-04-11 00:54:11 +0100

    fix api url noteEtudiantMoy Ctrl

--- src/main/java/com/example/demo/gestionNote/EtudiantMoyenne/NoteEtudiantMoyenneController.java
+++ src/main/java/com/example/demo/gestionNote/EtudiantMoyenne/NoteEtudiantMoyenneController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/etudiant-moyenne")
+@RequestMapping("api/isimm/chargeNote/EtudiantMoyenne")
 @CrossOrigin(origins = "*")
 public class NoteEtudiantMoyenneController {
 
